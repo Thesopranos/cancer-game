645f49f52a63df2ad8f1cffb1d6387cf2effd69d
Mertcan Cakir <zestillest@gmail.com>
2022-07-07 16:33:16 +0300

    tümörlü hücre görselleri eklendi, tümörlü hücreler eklendi, normal hücreler düzenlendi, normal hücre eklendi

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from random import randint
 pygame.init()
+import time
 
 class Core:
     def __init__(self):
@@ -17,11 +18,12 @@ class Core:
         self.player_X = 250
         self.player_Y = 600
 
-        self.normal_spam_time = 20
-        self.spam_counter = 0
-
+        self.spam_normal1 = False
+        self.spam_normal2 = True
         self.normal_animation_speed = 4
-        self.normal_free_animation_counter = 0
+        self.normal1_animation_counter = 0
+        self.normal2_animation_counter = 0
+
         self.normal_free = [pygame.image.load("img/normal-cell/normal-free/normal-free-1.png"),
                             pygame.image.load("img/normal-cell/normal-free/normal-free-2.png"),
                             pygame.image.load("img/normal-cell/normal-free/normal-free-3.png"),
@@ -60,18 +62,143 @@ class Core:
                             pygame.image.load("img/normal-cell/normal-spam/normal-spam-18.png"),
                             pygame.image.load("img/normal-cell/normal-spam/normal-spam-19.png")]
 
+        self.spam_tumor1 = False
+        self.spam_tumor2 = True
+        self.tumor_animation_speed = 4
+        self.tumor1_animation_counter = 0
+        self.tumor2_animation_counter = 0
+        self.tumor_spam_animation_counter = 0
+
+        self.tumor_free = [pygame.image.load("img/tumor-cell/tumor-free/tumor-free-1.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-2.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-3.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-4.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-5.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-6.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-7.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-8.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-9.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-10.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-11.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-12.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-13.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-14.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-15.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-16.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-17.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-18.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-19.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-20.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-21.png"),
+                           pygame.image.load("img/tumor-cell/tumor-free/tumor-free-22.png")]
+
+        self.tumor_spam = [pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-1.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-2.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-3.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-4.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-5.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-6.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-7.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-8.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-9.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-10.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-11.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-12.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-13.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-14.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-15.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-16.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-17.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-18.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-19.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-20.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-21.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-22.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-23.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-24.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-25.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-26.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-27.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-28.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-29.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-30.png"),
+                           pygame.image.load("img/tumor-cell/tumor-spam/tumor-spam-31.png")]
+
+    def normal_spam_free_animation(self, coo1, coo2):
+
+        if self.spam_normal1:
+            if self.normal1_animation_counter == len(self.normal_spam) * self.normal_animation_speed:
+                self.normal1_animation_counter = 0
+                self.spam_normal1 = False
+
+            self.window.blit(self.normal_spam[int(self.normal1_animation_counter // self.normal_animation_speed)],(coo1, coo2))
+
+        elif self.normal_spam_animation_counter == 0:
+            if self.normal1_animation_counter == len(self.normal_free) * self.normal_animation_speed:
+                self.normal1_animation_counter = 0
+                self.spam_normal1 = True
+
+            self.window.blit(self.normal_free[int(self.normal1_animation_counter // self.normal_animation_speed)],(coo1, coo2))
+
+        self.normal1_animation_counter += 1
+
+        if self.spam_normal2:
+            if self.normal2_animation_counter == len(self.normal_spam) * self.normal_animation_speed:
+                self.normal2_animation_counter = 0
+                self.spam_normal2 = False
+
+            self.window.blit(self.normal_spam[int(self.normal2_animation_counter // self.normal_animation_speed)],(coo1+ 250, coo2))
+
+        elif self.normal_spam_animation_counter == 0:
+            if self.normal2_animation_counter == len(self.normal_free) * self.normal_animation_speed:
+                self.normal2_animation_counter = 0
+                self.spam_normal2 = True
+
+            self.window.blit(self.normal_free[int(self.normal2_animation_counter // self.normal_animation_speed)],(coo1+ 250, coo2))
+
+        self.normal2_animation_counter += 1
+
+        if self.spam_tumor1:
+            if self.tumor1_animation_counter == len(self.tumor_spam) * self.tumor_animation_speed:
+                self.tumor1_animation_counter = 0
+                self.spam_tumor1 = False
+
+            self.window.blit(self.tumor_spam[int(self.tumor1_animation_counter // self.tumor_animation_speed)],(coo1 + 140, coo2))
+
+        elif self.tumor_spam_animation_counter == 0:
+            if self.tumor1_animation_counter == len(self.tumor_free) * self.tumor_animation_speed:
+                self.tumor1_animation_counter = 0
+                self.spam_tumor1 = True
+
+            self.window.blit(self.tumor_free[int(self.tumor1_animation_counter // self.tumor_animation_speed)],(coo1 + 140, coo2))
+
+        print(self.tumor1_animation_counter, self.spam_tumor1)
+        self.tumor1_animation_counter += 1
+
+        if self.spam_tumor2:
+            if self.tumor2_animation_counter == len(self.tumor_spam) * self.tumor_animation_speed:
+                self.tumor2_animation_counter = 0
+                self.spam_tumor2 = False
+
+            self.window.blit(self.tumor_spam[int(self.tumor2_animation_counter // self.tumor_animation_speed)],(coo1 + 390, coo2))
+
+        elif self.tumor_spam_animation_counter == 0:
+            if self.tumor2_animation_counter == len(self.tumor_free) * self.tumor_animation_speed:
+                self.tumor2_animation_counter = 0
+                self.spam_tumor2 = True
+
+            self.window.blit(self.tumor_free[int(self.tumor2_animation_counter // self.tumor_animation_speed)],(coo1 + 390, coo2))
+
+        print(self.tumor2_animation_counter, self.spam_tumor2)
+        self.tumor2_animation_counter += 1
     def draw(self):
+
 
         self.window.blit(self.background, (0,0))
 
+        self.normal_spam_free_animation(coo1 = 0, coo2 = 0)
+
         self.window.blit(self.player, (self.player_X, self.player_Y))
-
-        if self.normal_free_animation_counter == len(self.normal_free)*self.normal_animation_speed:
-            self.normal_free_animation_counter = 0
-
-        self.window.blit(self.normal_free[int(self.normal_free_animation_counter // self.normal_animation_speed)], (0, 0))
-
-        self.normal_free_animation_counter += 1
 
         self.clock.tick(60)
 
